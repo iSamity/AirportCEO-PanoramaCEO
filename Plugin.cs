818fbbae1cc85477dcdf0a6b80df0ae0d8115e45
iSamity <45425640+iSamity@users.noreply.github.com>
2026-01-27 23:24:45 +0100

    setup harmony patch

--- Plugin.cs
+++ Plugin.cs
@@ -1,4 +1,5 @@
-﻿using BepInEx;
+﻿using AirportCEOModLoader.WatermarkUtils;
+using BepInEx;
 using BepInEx.Configuration;
 using BepInEx.Logging;
 using PanoramaCEO.Config;
@@ -20,6 +21,37 @@ public class Plugin : BaseUnityPlugin
 
         DefaultConfig.Setup();
 
+        SetupHarmony();
+
         Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+    }
+
+    private void Start()
+    {
+        SetupModLoader();
+
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_NAME} - Finished start");
+    }
+
+
+    private void SetupHarmony()
+    {
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_NAME} - Setting up Harmony.");
+
+        var harmony = new HarmonyLib.Harmony(MyPluginInfo.PLUGIN_GUID);
+        harmony.PatchAll();
+
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_NAME} - Finished up Harmony.");
+    }
+
+    private void SetupModLoader()
+    {
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_NAME} - Setting up Mod Loader.");
+
+#if DEBUG
+        WatermarkUtils.Register(new WatermarkInfo(MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION, false));
+#else
+        WatermarkUtils.Register(new WatermarkInfo("PC", MyPluginInfo.PLUGIN_VERSION, true));
+#endif
     }
 }
